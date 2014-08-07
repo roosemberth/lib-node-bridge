228ce1fc0bf2fd71df5c6ce5e8b2859a432b466f
Marzell Camenzind <marzell.camenzind@gmail.com>
2014-08-07 19:57:08 +0200

    Extended MapUtils and exposed Db, MapUtils and Config

--- source/utils/MapUtils.js
+++ source/utils/MapUtils.js
@@ -40,6 +40,28 @@ var bfExecutor = function (tree, fn, callback) {
 };
 
 
+var syncBfExecutor = function (tree, fn) {
+
+  var bfs = function (node) {
+    if (node) {
+      if(fn(node)) {
+        for (var i = 0; i < l; ++i) {
+          bfs(tree[i]);
+        }
+      }
+    }
+  };
+
+  if (tree instanceof Array) {
+    for (var i = 0, l = tree.length; i < l; ++i) {
+      bfs(tree[i]);
+    }
+  } else {
+    bfs(tree);
+  }
+};
+
+
 /**
  * Returns true is the node is active and error-free
  * @param node from mapping
@@ -61,7 +83,9 @@ var updateUpdateTimestamp = function (map, callback) {
   bfExecutor(map, function (node, callback) {
     if (isActiveNode(node)) {
       if (node.updateCurrent) {
-        node.updateLast = node.updateCurrent;
+        if (!(!node.updateLast || (!!node.updateLast && node.updateLast > node.updateCurrent))) {
+          node.updateLast = node.updateCurrent;
+        }
       }
       return callback(true);
     } else {
@@ -71,9 +95,87 @@ var updateUpdateTimestamp = function (map, callback) {
 };
 
 
+var validateStream = function (stream) {
+  var error = null;
+  var valid = true;
+
+  valid = valid && stream.hasOwnProperty('name');
+  valid = valid && stream.hasOwnProperty('id');
+  valid = valid && stream.hasOwnProperty('uid');
+  valid = valid && stream.hasOwnProperty('active');
+  valid = valid && stream.hasOwnProperty('error');
+  valid = valid && stream.hasOwnProperty('view');
+  valid = valid && stream.hasOwnProperty('service');
+
+  if (stream.streams && stream.streams.length !== 0) {
+    for (var i = 0, ls = stream.streams.length; i < ls; ++i) {
+      var res_s = validateStream(stream.streams[i]);
+      if (!res_s.valid) {
+        return res_s;
+      }
+    }
+  }
+
+  if (stream.events && stream.events.length !== 0) {
+    for (var j = 0, le = stream.events.length; j < le; ++j) {
+      var res_e = validateEvent(stream.events[j]);
+      if (!res_e.valid) {
+        return res_e;
+      }
+    }
+  }
+
+
+  if (!valid) {
+    error = stream;
+  }
+  return {
+    valid: valid,
+    error: error
+  };
+};
+
+
+var validateEvent = function (event) {
+  var error = null;
+  var valid = true;
+
+  valid = valid && event.hasOwnProperty('streamId');
+  valid = valid && event.hasOwnProperty('uid');
+  valid = valid && event.hasOwnProperty('active');
+  valid = valid && event.hasOwnProperty('type');
+  valid = valid && event.hasOwnProperty('service');
+
+  if (!valid) {
+    error = event;
+  }
+  return {
+    valid: valid,
+    error: error
+  };
+};
+
+var validateMap = function (map) {
+  if (!map || map.length === 0) {
+    return false;
+  }
+  for (var i = 0, l = map.length; i < l; ++i) {
+    var res = validateStream(map[i]);
+    if (!res.valid) {
+      return res;
+    }
+  }
+  return {
+    valid: true,
+    error: null
+  };
+};
+
 module.exports = {
   bfExecutor: bfExecutor,
+  syncBfExecutor: syncBfExecutor,
   isActiveNode: isActiveNode,
   clearAllErrors: clearAllErrors,
-  updateUpdateTimestamp: updateUpdateTimestamp
+  updateUpdateTimestamp: updateUpdateTimestamp,
+  validateMap: validateMap
 };
